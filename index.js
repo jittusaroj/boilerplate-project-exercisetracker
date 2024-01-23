7aa8fb1f1f64d4b036673d7a67e6a3190c8df634
jitendra kumar saroj <saroj222137@gmail.com>
2024-01-23 22:47:55 +0530

    added path

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const moment = require('moment');
-
+const path = require("path");
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
